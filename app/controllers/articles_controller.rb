ff7da51b42faac1cba09cd11c6542bc5bb27a03a
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-14 10:59:50 +0100

    a

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret", only: [:new, :destroy]
+   
 
     def index
         @articles = Article.all
