468facfe0128d36baa22abf1d9f94d3510be5695
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-17 11:43:28 +0100

    fixed blank comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :body, length: {maximum: 100}
+  validates :body, length: {maximum: 100},presence: true
+  validates :commenter ,presence: true
 end
