80f4f033e44f8b31a13e1a8c0d43abbe00b4e3a4
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-24 17:07:19 +0100

    'nameFixed'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-    validates :name, length: {minimum: 5},uniqueness: true
+    validates :name, length: {minimum: 5},format: /\w+/i,uniqueness: true
     validates :email, format: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, confirmation: true
