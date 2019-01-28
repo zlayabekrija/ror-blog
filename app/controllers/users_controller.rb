428ec4ad9901b94320d1b92006a0748c12392253
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-28 10:07:55 +0100

    'get_routes_blocked'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    if logged_in?
     @user = User.find(params[:id])
+    else
+      flash[:danger] = 'No no no no'
+      redirect_to articles_path
+    end
   end
 
   def show
+    if logged_in?
     @user = User.find(params[:id])
     @articles = Article.where(user: @user.name)
+    else
+      flash[:danger] = 'Get ouuuuuut'
+      redirect_to articles_path
+    end
   end
 
   def update
