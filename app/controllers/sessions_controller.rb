3dfea87afd6ece906daf0ff71e9d234fb889b992
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-28 17:43:29 +0100

    'remember_me'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+      if @user && @user.authenticate(params[:session][:password])
         flash[:success] = 'Welcome'
-        log_in user
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
         redirect_to articles_path
       else
         flash[:danger] = 'Invalid email/password combination'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, confirmation: true
 
+
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
-      end
+    end
     
       # Returns a random token.
       def User.new_token
@@ -24,6 +25,12 @@ class User < ApplicationRecord
 
        # Returns true if the given token matches the digest.
       def authenticated?(remember_token)
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
       end
+
+      def forget
+        update_attribute(:remember_digest, nil)
+      end
+    
 end
