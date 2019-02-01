f11edc9437d72f80cdeb0772d65426eb9cebf4da
zlayabekrija <zlaja.bekrija@gmail.com>
2019-02-01 12:17:20 +0100

    'img'

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,20 +1,20 @@
-CarrierWave.configure do |config|
-    if Rails.env.development?
-      config.storage :file
-      config.asset_host = 'http://localhost:3000'
-    else
-      config.storage = :fog
-      config.fog_use_ssl_for_aws = true
-      config.fog_directory  = ENV['FOG_DIRECTORY']
-      config.fog_public     = true
-      config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
-      config.asset_host = 'https://s3.amazonaws.com/website'
+# CarrierWave.configure do |config|
+#     if Rails.env.development?
+#       config.storage :file
+#       config.asset_host = 'http://localhost:3000'
+#     else
+#       config.storage = :fog
+#       config.fog_use_ssl_for_aws = true
+#       config.fog_directory  = ENV['FOG_DIRECTORY']
+#       config.fog_public     = true
+#       config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
+#       config.asset_host = 'https://s3.amazonaws.com/website'
   
-      config.fog_credentials = {
-        provider:               ENV['FOG_PROVIDER'],
-        aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-        path_style:             ENV['FOG_PATH_STYLE']
-      }
-    end
-  end
+#       config.fog_credentials = {
+#         provider:               ENV['FOG_PROVIDER'],
+#         aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+#         aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+#         path_style:             ENV['FOG_PATH_STYLE']
+#       }
+#     end
+#   end
