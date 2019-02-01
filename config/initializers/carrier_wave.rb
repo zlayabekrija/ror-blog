72c2e87ad20315b64afe746793b5967f4695f7e1
zlayabekrija <zlaja.bekrija@gmail.com>
2019-02-01 12:14:14 +0100

    'img'

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,20 @@
-if Rails.env.production?
-    CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+    if Rails.env.development?
+      config.storage :file
+      config.asset_host = 'http://localhost:3000'
+    else
+      config.storage = :fog
+      config.fog_use_ssl_for_aws = true
+      config.fog_directory  = ENV['FOG_DIRECTORY']
+      config.fog_public     = true
+      config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
+      config.asset_host = 'https://s3.amazonaws.com/website'
+  
       config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        provider:               ENV['FOG_PROVIDER'],
+        aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+        path_style:             ENV['FOG_PATH_STYLE']
       }
-      config.fog_directory     =  ENV['S3_BUCKET']
     end
   end
