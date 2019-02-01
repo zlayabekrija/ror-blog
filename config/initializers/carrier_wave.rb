a58f05ce29bd0043323b05d221421542a5520273
zlayabekrija <zlaja.bekrija@gmail.com>
2019-02-01 12:47:52 +0100

    'img'

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
     CarrierWave.configure do |config|
       config.aws_credentials = {
-       # Configuration for Amazon S3
+       # Configuration for Amazon AWS
   
-      access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
-      secret_access_key: ENV.fetch('S3_SECRET_KEY'),
-      region:            ENV.fetch('S3_REGION') # Required
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+      secret_access_key: ENV.fetch('AWS_SECRET_KEY'),
+      region:            ENV.fetch('AWS_REGION') # Required
   }
       config.storage    = :aws
-      config.aws_bucket = ENV.fetch('S3_BUCKET')  
+      config.aws_bucket = ENV.fetch('AWS_BUCKET')  
     end
   end
